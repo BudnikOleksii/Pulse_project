e9423bb134d07a8fa9ad4e0dd1209fa9d11a061c
Alex <budnikoleksii@gmail.com>
2020-05-25 14:56:05 +0300

    updated forms

--- src/js/script.js
+++ src/js/script.js
@@ -193,49 +193,58 @@ window.addEventListener('DOMContentLoaded', () => {
     const forms = document.querySelectorAll('form');
 
     const message = {
-        loading: 'Загрузка',
+        loading: 'Загрузка...',
         success: 'Спасибо за вашу заявку! Наш менеджер свяжется с вами в ближайшее время!',
-        failure: 'Что-то пошло не так...'
+        failure: 'Что-то пошло не так...',
+        spinner: 'icons/forms/spinner.svg',
+        ok:'icons/forms/ok.png',
+        fail:'icons/forms/fail.png'
     };
 
-    forms.forEach(item => postData(item));
+    const postData = async (url, data) => {
+        let res = await fetch(url, {
+            method: "POST",
+            body: data
+        });
 
-    function postData(form) {
+        return await res.text();
+    };
+
+    forms.forEach(form => {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
 
-            const statusMessage = document.createElement('div');
+            let statusMessage = document.createElement('div');
             statusMessage.classList.add('status');
-            statusMessage.textContent = message.loading;
-            form.append(statusMessage);
+            form.parentNode.append(statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
+            let statusImg = document.createElement('img');
+            statusImg.setAttribute('src', message.spinner);
+            statusImg.classList.add('animated', 'fadeInUp');
+            statusMessage.append(statusImg);
 
-            request.setRequestHeader('content-type', 'application/json');
+            let textMessage = document.createElement('div');
+            textMessage.textContent = message.loading;
+            statusMessage.append(textMessage);
+
             const formData = new FormData(form);
 
-            const object = {};
-            formData.forEach(function(value, key) {
-                object[key] = value;
-            });
-
-            const json = JSON.stringify(object);
-
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    statusMessage.textContent = message.success;
+            postData('server.php', formData)
+                .then(res => {
+                    console.log(res);
+                    statusImg.setAttribute('src', message.ok);
+                    textMessage.textContent = message.success;
+                })
+                .catch(() => {
+                    statusImg.setAttribute('src', message.fail);
+                    textMessage.textContent = message.failure;
+                })
+                .finally(() => {
                     form.reset();
                     setTimeout(() => {
                         statusMessage.remove();
                     }, 2000);
-                } else {
-                    statusMessage.textContent = message.failure;
-                }
-            });
+                });
         });
-    }
+    });
 });
