fd6a4621617c1cec19a64e475f4e2bc107755352
Alex <budnikoleksii@gmail.com>
2020-05-25 07:29:25 +0300

    added script for tabs section

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
-  // Slider
+    // Slider
   const prev = document.querySelector('.carousel__prev'),
         next = document.querySelector('.carousel__next'),
         slides = document.querySelectorAll('.carousel__slide');
@@ -54,4 +54,57 @@ window.addEventListener('DOMContentLoaded', () => {
     });
     
     showSlides(slideIndex);
+
+    // Tabs
+    const tabs = document.querySelectorAll('.catalog__tab'),
+          tabsContent = document.querySelectorAll('.catalog__content'),
+          tabsParent = document.querySelector('.catalog__tabs');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.remove('catalog__content_active');
+            item.classList.add('animated', 'fadeInUp');
+        });
+        tabs.forEach(item => {
+            item.classList.remove('catalog__tab_active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('catalog__content_active');
+        tabs[i].classList.add('catalog__tab_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (e) => {
+        const target = e.target;
+
+        if (target && target.parentNode.classList.contains('catalog__tab')) {
+            tabs.forEach((item, i) => {
+                if (target.parentNode == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+
+    const moreLink = document.querySelectorAll('.catalog-item__link'),
+          moreBack = document.querySelectorAll('.catalog-item__back'),
+          itemContent = document.querySelectorAll('.catalog-item__content'),
+          moreList = document.querySelectorAll('.catalog-item__list');
+
+    function toggleMoreList (trigger) {
+        trigger.forEach((link, i) => {
+            link.addEventListener('click', (e) => {
+                e.preventDefault();
+                itemContent[i].classList.toggle('catalog-item__content_active');
+                moreList[i].classList.toggle('catalog-item__list_active');
+            });
+        });
+    }
+    toggleMoreList(moreLink);
+    toggleMoreList(moreBack);
 });
