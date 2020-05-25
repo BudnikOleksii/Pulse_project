8da378d5c0384a16e617f7d41473d852dbfa9c75
Alex <budnikoleksii@gmail.com>
2020-05-25 16:08:09 +0300

    added validation mask script

--- src/js/script.js
+++ src/js/script.js
@@ -253,4 +253,69 @@ window.addEventListener('DOMContentLoaded', () => {
                 });
         });
     });
+
+    // Validation mask
+    function checkEmailsInputs() {
+        const emailInputs = document.querySelectorAll('[name="email"]');
+
+        emailInputs.forEach(input => {
+            input.addEventListener('keypress', (e) => {
+                if (e.key.match(/[^a-z 0-9 @ \.]/ig)) {
+                    e.preventDefault();
+                }
+            });
+        });
+    }
+
+    checkEmailsInputs();
+
+    function validationMask() {
+        let setCursorPosition = (pos, elem) => {
+            elem.focus();
+
+            if (elem.setSelectionRange) {
+                elem.setSelectionRange(pos, pos);
+            } else if (elem.createTextRange) {
+                let range = elem.createTextRange();
+                
+                range.collapse(true);
+                range.moveEnd('character', pos);
+                range.moveStart('character', pos);
+                range.select();
+            }
+        };
+
+        function createMask(event) {
+            let matrix = '+38 (___) ___ __ __',
+                i = 0,
+                def = matrix.replace(/\D/g, ''),
+                val = this.value.replace(/\D/g, '');
+
+            if (def.length >= val.length) {
+                val = def;
+            }
+
+            this.value = matrix.replace(/./g, function(a) {
+                return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
+            });
+
+            if (event.type === 'blur') {
+                if (this.value.length == 2) {
+                    this.value = '';
+                }
+            } else {
+                setCursorPosition(this.value.length, this);
+            }
+        }
+        
+        const inputs = document.querySelectorAll('[name="phone"]');
+
+        inputs.forEach(input => {
+            input.addEventListener('input', createMask);
+            input.addEventListener('focus', createMask);
+            input.addEventListener('blur', createMask);
+        });
+    }
+
+    validationMask();
 });
