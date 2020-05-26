8fdc464af48a8172f8263735ce1530faf9805c8b
Alex <budnikoleksii@gmail.com>
2020-05-26 10:33:14 +0300

    added script for scroll

--- src/js/script.js
+++ src/js/script.js
@@ -318,4 +318,50 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     validationMask();
+
+    // Scrolling with request animation frame
+    const upElem = document.querySelector('.pageup');
+
+    window.addEventListener('scroll', () => {
+        if (document.documentElement.scrollTop > 1650) {
+            upElem.classList.add('animated', 'fadeIn');
+            upElem.classList.remove('fadeOut');
+        } else {
+            upElem.classList.add('fadeOut');
+            upElem.classList.remove('fadeIn');
+        }
+    });
+
+    let links = document.querySelectorAll('[href^="#"]'),
+        speed = 0.3;
+
+    links.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            let widthTop = document.documentElement.scrollTop,
+                hash = this.hash,
+                toBlock = document.querySelector(hash).getBoundingClientRect().top,
+                start = null;
+
+            requestAnimationFrame(step);
+
+            function step(time) {
+                if (start === null) {
+                    start = time;
+                }
+
+                let progress = time -start,
+                    r = (toBlock < 0 ? Math.max(widthTop - progress/speed, widthTop + toBlock) : Math.min(widthTop + progress/speed, widthTop + toBlock));
+
+                document.documentElement.scrollTo(0, r);
+
+                if (r != widthTop + toBlock) {
+                    requestAnimationFrame(step);
+                } else {
+                    location.hash = hash;
+                }
+            }
+        });
+    });
 });
